6ee0e904d6863e7d626ef68cddeff0805f35c7cd
Rubén Katins Durán <83010981+rkatins@users.noreply.github.com>
2023-06-03 13:12:17 +0200

    Update fprintf.c

--- Empezar en C/Ficheros/Linea Por Linea/fprintf.c
+++ Empezar en C/Ficheros/Linea Por Linea/fprintf.c
@@ -15,8 +15,9 @@ main(){
 	
 	printf("Escribe algo:");
 	gets(linea);
-
-	fprintf(fichero,"%s",linea);  // fprintf escribe en relacion a lo indicado en el array[80] correspondiente a la variable char linea
+	
+	// fprintf escribe en relacion a lo indicado en el array[80] correspondiente a la variable char linea
+	fprintf(fichero,"%s",linea);  
 	
 	fclose(fichero);
 }
