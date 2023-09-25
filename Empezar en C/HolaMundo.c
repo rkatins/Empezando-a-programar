b78e53cb040aeb2d97d8c87531b15f8fcd158385
Rubén Katins Durán <83010981+rkatins@users.noreply.github.com>
2023-09-25 16:30:27 +0200

    Hola Mundo

---
+++ Empezar en C/HolaMundo.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+/*
+  - Autor:         	rkatins
+  - Correo:			katinsruben.98@gmial.com
+  - Descripcion:	Hola Mundo
+*/
+
+int main() {
+ 	printf( "Hola Mundo\n" );	// Esto imprime hola mundo en pantalla
+ 
+ 	return 0; 
+ } 
+
